9e809476e9841f77e083ca87a10e2b43fb7482c1
Chann44 <44chansw@gmail.com>
2022-06-27 17:28:59 +0530

    intial setup done

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Login from './Login'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
+
+export default Home
